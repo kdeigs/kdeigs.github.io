12779b234cf2fcac32d2a5a7557b95b32ac659ab
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-02-08 08:36:49 -0600

    Commented code and refactored dealer function

--- blackjack-app/js/app.js
+++ blackjack-app/js/app.js
@@ -4,33 +4,43 @@ let playerBust = false;
 let dealerBust = false;
 
 const deal = () => { //Deals Cards to Player and Dealer
-    let $playerCards = $('#playerCards');
-    let $buttons = $('#buttons')
-    $('#deal').remove();
+    $('#deal').remove(); //Removes Deal Button
+    playerDeal();
+    dealerDeal();
+    addHitStayButtons();
+}
 
+const addHitStayButtons = () => { //Adds hit and stay buttons to the page
     let $hitButton = $('<button>').text('Hit').attr('id', 'hit');
     $hitButton.on('click', hit);
+
     let $stayButton = $('<button>').text('Stay').attr('id', 'stay');
     $stayButton.on('click', stay);
 
-    let randomCard = cards[Math.floor(Math.random()*12)];
-    let $cardOne = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
-    randomCard = cards[Math.floor(Math.random()*12)];
-    let $cardTwo = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
-    randomCard = cards[Math.floor(Math.random()*12)];
-
-    dealerDeal();
-
-    $playerCards.append($cardOne, $cardTwo);
-    $buttons.append($hitButton, $stayButton);
+    $('#buttons').append($hitButton, $stayButton);
 }
 
-const dealerDeal = () => {
+const playerDeal = () => { //Deals 2 cards to the player
+    let randomCard = cards[Math.floor(Math.random()*12)];
+    let $cardOne = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
+
+    randomCard = cards[Math.floor(Math.random()*12)];
+    let $cardTwo = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
+
+    ('#playerCards').append($cardOne, $cardTwo);
+}
+
+const dealerInitialDeal = () => { //Deals the initial 2 cards to the dealer
     let randomCard = cards[Math.floor(Math.random()*12)];
     let $dealerCardOne = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
+
     randomCard = cards[Math.floor(Math.random()*12)];
     let $dealerCardTwo = $('<div>').addClass('cardBack').attr('id', randomCard);
+
     $('#dealerCards').append($dealerCardOne, $dealerCardTwo);
+}
+
+const dealerPlay = () => { //Deals cards until the dealer hits 17 or higher
     while(calculate($('#dealerCards')) < 17){
         randomCard = cards[Math.floor(Math.random()*12)];
         let $dealerCard = $('<div>').addClass('cardBack').attr('id', randomCard);
@@ -39,28 +49,28 @@ const dealerDeal = () => {
     if(calculate($('#dealerCards')) > 21){
         dealerBust = true;
     }
-    
 }
 
-const hit = () => {
+const hit = () => { //hits the player with another card if they haven't already bust
     if(!playerBust){
         let randomCard = cards[Math.floor(Math.random()*12)];
         let $card = $('<div>').text(randomCard).addClass('card').attr('id', randomCard);
+
         $('#playerCards').append($card);
         if(calculate($('#playerCards')) > 21){
-            bust();
+            playerBust = true;
         }
     }
 }
 
-const showDealer = () => {
+const showDealer = () => { //Reveals the dealers cards that are currently hidden
     console.log($('#dealerCards').children());
     for(let item of ($('#dealerCards').children())){
         $(item).text($(item).attr('id')).removeClass('cardBack').addClass('card');
     }
 }
 
-const reset = () => {
+const reset = () => { //Resets the game and adds a deal button 
     $('#hit').remove();
     $('#stay').remove();
 
@@ -75,27 +85,34 @@ const reset = () => {
     $('#buttons').append($deal);
 }
 
-const bust = () => {
+const bust = () => { //Dead function. Need to refactor into different functions
     playerBust = true;
     showDealer();
     showWinner();
     reset();
 }
 
-const showWinner = () => {
+const showWinner = () => { //Displays the winnner on the screen using the calcWin function
     let winner = calcWin();
-    $winnerCard = $('<h2>').text(`The ${winner} is the winner!`).attr('id', 'winner');
+    let $winnerCard;
+
+    if(winner === 'tie'){
+        $winnerCard = $('<h2>').text(`Its a tie!`).attr('id', 'winner');
+    }else{
+        $winnerCard = $('<h2>').text(`The ${winner} is the winner!`).attr('id', 'winner');
+    }
+    
     $('#winnerCard').append($winnerCard);
 }
 
-const stay = () => {
+const stay = () => { //Needs to be refactored into different functions
     calculate($('#playerCards'));
     showWinner();
     showDealer();
     reset();
 }
 
-const calculate = (cardStack) => {
+const calculate = (cardStack) => { //Calculates the total in a cardStack (player or dealer) and returns the current score of the hand
     $cardDivs = cardStack.children()
     let max = 0;
     let min = 0;
@@ -113,7 +130,6 @@ const calculate = (cardStack) => {
             min += parseInt(itemId);
         }
     }
-    console.log(`Min: ${min} Max: ${max}`);
     if(max <= 21){
         return max;
     }else{
@@ -121,17 +137,20 @@ const calculate = (cardStack) => {
     }
 }
 
-const calcWin = () => {
+const calcWin = () => { //Calculates who the winner is and returns them
     if(dealerBust && !playerBust){
         return 'player';
     }else if (!dealerBust && playerBust){
         return 'dealer'
     }else if(calculate($('#playerCards')) > calculate($('#dealerCards'))){
         return 'player';
+    }else if(calculate($('#playerCards')) === calculate($('#dealerCards'))){
+        return 'tie';
     }else{
-        return 'dealer'
+        return 'dealer';
     }
 }
+
 $(() => {
-    $('#deal').on('click', deal);
+    $('#deal').on('click', deal); //Sets up the deal button
 });
