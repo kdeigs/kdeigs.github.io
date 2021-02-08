c410d3c2f2bd8c26d2a97ff09fe098901b511080
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-02-08 09:54:28 -0600

    Added home.html boilerplate and started main.css styling

--- blackjack-app/js/app.js
+++ blackjack-app/js/app.js
@@ -1,3 +1,5 @@
+//Color schemes from https://visme.co/blog/website-color-schemes/
+
 const cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const suits = ['C', 'D', 'H', 'S'];
 const colors = ['blue', 'gray', 'green', 'purple', 'red', 'yellow'];
