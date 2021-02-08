df6ae3fab9e2237f549aa0ec35ff25a2991a18e8
Kole Deighan <kdeigs@Koles-MacBook-Air.local>
2021-02-08 17:27:08 -0600

    Added PLayerTurn functionality to simon says

--- blackjack-app/js/simonSays.js
+++ blackjack-app/js/simonSays.js
@@ -1,5 +1,6 @@
 let currentComputerSequence = [];
 let currentPlayerSequence = [];
+
 const startGame = () => {
     $('#start').remove();
     $('#counter').text('3');
@@ -16,14 +17,15 @@ const startGame = () => {
 }
 
 const createSequence = (len) => {
-    currentComputerSequence = [];
+    currentComputerSequence = []
     for(let i = 0; i < len; i++){
-        currentComputerSequence.push(Math.floor(Math.random()*4));
+        currentComputerSequence.push((Math.floor(Math.random()*4)));
     }
     displaySequence(0);
 }
 
 const displaySequence = (currentIndex) => {
+    console.log(currentComputerSequence);
     switch(currentComputerSequence[currentIndex]){
         case 0:
             $('#green').css('background-color', 'lightGreen');
@@ -47,6 +49,7 @@ const displaySequence = (currentIndex) => {
 const nextSequence = (currentIndex) => {
     resetColors();
     if(currentComputerSequence.length === currentIndex+1){
+        currentPlayerSequence = [];
         playerTurn();
     }else{
         displaySequence(currentIndex++);
@@ -54,44 +57,78 @@ const nextSequence = (currentIndex) => {
 }
 
 const playerTurn = () => {
-    let currentPlayerSequence = [];
+    $('#green').off();
+    $('#red').off();
+    $('#yellow').off();
+    $('#blue').off();
     $('#green').on('click', () => {
-        if(currentPlayerSequence.length < currentComputerSequence.length){
-            resetColors();
-            $('#green').css('background-color', 'lightGreen');
-            currentPlayerSequence.push(0);
+        resetColors();
+        $('#green').css('background-color', 'lightGreen');
+        currentPlayerSequence.push(0);
+        console.log(0);
+        if(currentPlayerSequence.length === currentComputerSequence.length){
+            setTimeout(compareAnswers, 1000);
         }
     });
     $('#red').on('click', () => {
-        if(currentPlayerSequence.length < currentComputerSequence.length){
-            resetColors();
-            $('#red').css('background-color', 'pink');
-            currentPlayerSequence.push(1);
+        resetColors();
+        $('#red').css('background-color', 'pink');
+        currentPlayerSequence.push(1);
+        console.log(1);
+        if(currentPlayerSequence.length === currentComputerSequence.length){
+            setTimeout(compareAnswers, 1000);
         }
     });
     $('#yellow').on('click', () => {
-        if(currentPlayerSequence.length < currentComputerSequence.length){
-            resetColors();
-            $('#yellow').css('background-color', 'lightYellow');
-            currentPlayerSequence.push(2);
+        resetColors();
+        $('#yellow').css('background-color', 'lightYellow');
+        currentPlayerSequence.push(2);
+        console.log(2);
+        if(currentPlayerSequence.length === currentComputerSequence.length){
+            setTimeout(compareAnswers, 1000);
         }
     });
     $('#blue').on('click', () => {
-        if(currentPlayerSequence.length < currentComputerSequence.length){
-            resetColors();
-            $('#blue').css('background-color', 'lightBlue');
-            currentPlayerSequence.push(3);
+        resetColors();
+        $('#blue').css('background-color', 'lightBlue');
+        currentPlayerSequence.push(3);
+        console.log(3);
+        if(currentPlayerSequence.length === currentComputerSequence.length){
+            setTimeout(compareAnswers, 1000);
         }
     });
-
-    while(currentPlayerSequence.length < currentComputerSequence.length){
-        setTimeout(() =>{}, 100);
-    }
-
-    compareAnswers();
 }
 
-const compareAnswers()
+const compareAnswers = () => {
+    console.log(currentPlayerSequence);
+    resetColors();
+    let won = true;
+    for(let i = 0; i < currentPlayerSequence.length; i++){
+        if(currentPlayerSequence[i] != currentComputerSequence[i]){
+            won = false;
+        }
+    }
+    if(won){
+        $('#counter').text('Correct!');
+        setTimeout(() => {
+            $('#counter').text('3');
+            setTimeout(() => {
+                $('#counter').text('2');
+                setTimeout(() => {
+                    $('#counter').text('1');
+                    setTimeout(() => {
+                        $('#counter').text('');
+                        createSequence(currentPlayerSequence.length++);
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }else{
+        $('#counter').text('Sorry you were incorrect!');
+        const $playAgain = $('<button>').text('Play Again').on('click', startGame).attr('id', 'start');
+        $('#container').append($playAgain);
+    }
+}
 
 const resetColors = () => {
     $('#green').css('background-color', 'green');
